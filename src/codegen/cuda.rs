951a6ee312b2075d6d4c50f24453fa352e155db7
kcvlex <krkr9893@gmail.com>
2026-01-04 18:27:54 +0900

    delete dbg

--- src/codegen/cuda.rs
+++ src/codegen/cuda.rs
@@ -1167,7 +1167,6 @@ impl<'sched> HostCodeGenerator<'sched> {
 
                         let ndim = ty.dims.ndim();
                         let row = ty.dims[ndim - 2];
-                        dbg!(&ty, &kernel);
                         assert!(ty.strides()[ndim - 1] == row);
                         assert!(ty.strides()[ndim - 2] == 1);
                         true
