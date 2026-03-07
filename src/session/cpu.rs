a630293434a1bfbbb0ff2f5b5fa902c3f1c8ff81
kcvlex <krkr9893@gmail.com>
2026-03-08 07:34:41 +0900

    [CPU] Remove dependency to clang

--- src/session/cpu.rs
+++ src/session/cpu.rs
@@ -1,8 +1,9 @@
 use std::path::Path;
-use std::process::Command;
 
 use inkwell::context::Context;
-use inkwell::targets::FileType;
+use inkwell::execution_engine::ExecutionEngine;
+use inkwell::support::load_library_permanently;
+use inkwell::OptimizationLevel;
 use itertools::zip_eq;
 use log::info;
 use rayon::prelude::*;
@@ -17,8 +18,6 @@ use crate::tensor::Tensor;
 
 type CodeType = unsafe extern "C" fn(*const *mut u8, *const *const u8, *const *const u8);
 
-const DEBUG: bool = true;
-
 pub struct SessionCPU {
     #[allow(dead_code)]
     input_ty: Vec<ResolvedTensorType>,
@@ -28,8 +27,11 @@ pub struct SessionCPU {
     #[allow(dead_code)]
     codegen_ctx: CodeGenContext,
 
+    // engine must be dropped before contexts (field drop order guarantees this)
     #[allow(dead_code)]
-    lib: libloading::Library,
+    _engine: ExecutionEngine<'static>,
+    #[allow(dead_code)]
+    _contexts: Vec<Context>,
     func: CodeType,
 }
 
@@ -43,94 +45,102 @@ impl SessionCPU {
         build_dir: &Path,
     ) -> Result<Self, SessionError> {
         let codegen_ctx = CodeGenContext::new(schedule).map_err(SessionError::CodeGenError)?;
-        let (codegens, mut contexts): (Vec<_>, Vec<_>) = codegen_ctx
-            .all_necessary_kernels()
-            .iter()
-            .copied()
-            .map(|id| {
-                let ll_ctx = Context::create();
-                (id, ll_ctx)
-            })
-            .unzip();
-        contexts.push(Context::create());
-        let mut codegens = codegens
-            .into_iter()
-            .enumerate()
-            .map(|(i, id)| {
-                let ll_ctx = &contexts[i];
-                codegen_ctx.new_codegen_for_kernel(id, ll_ctx)
-            })
-            .collect::<Result<Vec<_>, _>>()
-            .map_err(SessionError::CodeGenError)?;
-        codegens.push({
-            let ll_ctx = contexts.last().unwrap();
-            codegen_ctx
-                .new_codegen_for_main(ll_ctx)
-                .map_err(SessionError::CodeGenError)?
-        });
+        let kernel_ids = codegen_ctx.all_necessary_kernels();
 
-        let codegens = codegens
-            .par_iter()
-            .enumerate()
-            .map(|(i, codegen)| {
-                let path = build_dir.join(format!("model_{i}.o"));
-                (path, codegen)
-            })
-            .collect::<Vec<_>>();
-
-        let shared_obj = build_dir.join("model.so");
+        let mut contexts: Vec<Context> = kernel_ids.iter().map(|_| Context::create()).collect();
 
         info!("Compiling");
-        let objs = codegens
-            .into_par_iter()
-            //.into_iter()
-            .map(|(path, codegen)| {
-                codegen.compile().unwrap();
-                if !DEBUG {
-                    codegen.run_opt_aggressive().unwrap();
-                }
-                if opt.save_build_dir {
-                    let ll_path = path.with_extension("ll");
-                    codegen.module().print_to_file(&ll_path).unwrap();
-                }
-                codegen.write_to_file(FileType::Object, &path).unwrap();
-                path
+
+        let codegens = kernel_ids
+            .iter()
+            .zip(contexts.iter())
+            .map(|(&id, ctx)| codegen_ctx.new_codegen_for_kernel(id, ctx))
+            .collect::<Result<Vec<_>, _>>()
+            .map_err(SessionError::CodeGenError)?;
+
+        codegens
+            .par_iter()
+            .try_for_each(|cg| -> Result<(), String> {
+                cg.compile().map_err(|e| format!("{e:?}"))?;
+                //cg.run_opt_aggressive().map_err(|e| format!("{e:?}"))?;
+                Ok(())
             })
-            .collect::<Vec<_>>();
-        info!("Compiled");
+            .map_err(SessionError::OtherError)?;
 
-        // TODO: args
-        // TODO: remove -lm after llvm.tanh.* is available
-        Command::new("clang")
-            .args([
-                "-shared",
-                "-fPIC",
-                "-fopenmp",
-                "-I/usr/include/openblas",
-                "-lopenblas",
-                "-lm",
-                "-o",
-                shared_obj.to_str().unwrap(),
-            ])
-            .args(objs.iter().map(|p| p.to_str().unwrap()))
-            .status()
-            .map_err(|e| SessionError::OtherError(format!("{:?}", e)))?;
+        let kernel_modules: Vec<_> = codegens
+            .into_iter()
+            .enumerate()
+            .map(|(i, cg)| {
+                if opt.save_build_dir {
+                    let ll_path = build_dir.join(format!("kernel_{}.ll", kernel_ids[i].index()));
+                    cg.module().print_to_file(&ll_path).unwrap();
+                }
+                cg.into_module()
+            })
+            .collect();
 
-        info!("Generated");
+        // Generate main module
+        let main_context = Context::create();
+        let main_codegen = codegen_ctx
+            .new_codegen_for_main(&main_context)
+            .map_err(SessionError::CodeGenError)?;
+        main_codegen.compile().map_err(SessionError::CodeGenError)?;
+        main_codegen
+            .run_opt_aggressive()
+            .map_err(SessionError::CodeGenError)?;
+        if opt.save_build_dir {
+            let ll_path = build_dir.join("main.ll");
+            main_codegen.module().print_to_file(&ll_path).unwrap();
+        }
+        let main_module = main_codegen.into_module();
 
-        let lib = unsafe { libloading::Library::new(shared_obj.as_os_str()) }
-            .map_err(|e| SessionError::OtherError(format!("{:?}", e)))?;
-        let func: libloading::Symbol<CodeType> = unsafe { lib.get(b"main") }
-            .map_err(|e| SessionError::OtherError(format!("{:?}", e)))?;
-        let func = *func;
+        info!("Load external libraries");
+        for lib in &["libopenblas.so", "libomp.so"] {
+            load_library_permanently(Path::new(lib)).map_err(|e| {
+                SessionError::OtherError(format!("Failed to load {}: {:?}", lib, e))
+            })?;
+        }
 
-        info!("Loaded");
+        info!("Linking and JIT compiling");
+        let engine = main_module
+            .create_jit_execution_engine(OptimizationLevel::Aggressive)
+            .map_err(|e| {
+                SessionError::OtherError(format!("Failed to create JIT engine: {:?}", e))
+            })?;
+
+        // Transmute engine to 'static before adding cross-context modules
+        // SAFETY: _contexts outlives _engine by field drop order
+        let engine: ExecutionEngine<'static> = unsafe { std::mem::transmute(engine) };
+
+        // Add kernel modules (transmute lifetime to match engine's 'static)
+        for module in kernel_modules.iter() {
+            // SAFETY: corresponding context is stored in _contexts and outlives _engine
+            engine
+                .add_module(unsafe { std::mem::transmute(module) })
+                .map_err(|()| {
+                    SessionError::OtherError("Failed to add module to JIT engine".to_string())
+                })?;
+        }
+
+        // Get function pointer
+        let func_addr = engine.get_function_address("main").map_err(|e| {
+            SessionError::OtherError(format!("Failed to get 'main' function: {:?}", e))
+        })?;
+        let func: CodeType = unsafe { std::mem::transmute(func_addr) };
+
+        info!("JIT compiled and loaded");
+
+        // Drop module objects to release borrows on contexts. The underlying LLVM modules are already owned by the engine (via create_jit_execution_engine and add_module).
+        drop(kernel_modules);
+        drop(main_module);
+        contexts.push(main_context);
 
         Ok(Self {
             input_ty,
             output_ty,
             codegen_ctx,
-            lib,
+            _engine: engine,
+            _contexts: contexts,
             func,
             initializer,
         })
